c8e7dc3f73670280c64645da49ee53906fd24b6b
Sarthak Jariwala <jariwala@uw.edu>
2020-12-20 23:57:04 -0800

    add SolarDatasetQuery

--- nrel_dev_api/_nbdev.py
+++ nrel_dev_api/_nbdev.py
@@ -4,7 +4,8 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"SolarResourceData": "00_solar.ipynb",
          "BASE_URL": "00_solar.ipynb",
-         "PVWattsV6": "00_solar.ipynb"}
+         "PVWattsV6": "00_solar.ipynb",
+         "SolarDatasetQuery": "00_solar.ipynb"}
 
 modules = ["solar.py"]
 
